8d91b825acc78b279e2e42777ae03479045d4d84
potchakik <santhonydan7@yahoo.com>
2022-02-09 05:20:45 +0800

    ADD carousel

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import ImageSlider from "../ImageSlider/ImageSlider";
 
 import { Container } from "./styles";
 
 const Home = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <ImageSlider />
+    </Container>
+  );
 };
 
 export default Home;
