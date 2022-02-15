59f0d83a670f95c0ee685b9aae206be724dc3ee9
potchakik <santhonydan7@yahoo.com>
2022-02-15 14:36:29 +0800

    ADD Details components

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import {
+  Container,
+  Controls,
+  Background,
+  ImgTitle,
+  PlayButton,
+  TrailerButton,
+  AddButton,
+  GroupWatchButton,
+} from "./style";
 
 const style = () => {
-  return <div>Detail</div>;
+  return (
+    <Container>
+      <Background>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.7gAE7LptUycxYmScGs6LdQHaEK%26pid%3DApi&f=1" />
+      </Background>
+      <ImgTitle>
+        <img src="" />
+      </ImgTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" />
+        </GroupWatchButton>
+      </Controls>
+    </Container>
+  );
 };
 
 export default style;
---
+++ src/components/Detail/style.js
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImgTitle = styled.div`
+  height: 30vh;
+  width: 35vw;
+  min-height: 170px;
+  min-width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  padding: 0px 24px;
+  border-radius: 4px;
+  margin-right: 22px;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+const GroupWatchButton = styled(AddButton)`
+  background-color: rgb(0, 0, 0);
+`;
+
+export {
+  Container,
+  Background,
+  ImgTitle,
+  Controls,
+  PlayButton,
+  TrailerButton,
+  AddButton,
+  GroupWatchButton,
+};
