7489fd72c7226c50703e2bb3cc12d6b249753c5f
potchakik <santhonydan7@yahoo.com>
2022-02-19 04:15:44 +0800

    ADD Login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-import { Container } from "./style";
+import {
+  Container,
+  CTA,
+  CTALogoOne,
+  SignUp,
+  Description,
+  CTALogoTwo,
+} from "./style";
 
 const Login = () => {
-  return <Container>Login</Container>;
+  return (
+    <Container>
+      <CTA>
+        <CTALogoOne src="/images/cta-logo-one.svg" />
+        <SignUp>GET ALL HERE</SignUp>
+        <Description>
+          Get Premier Access to Raya and the Last Dragon for with a Disney+
+          subscription. As of 03/26/21, the price and the Disney Bundle will
+          increase by $1
+        </Description>
+        <CTALogoTwo src="/images/cta-logo-two.png" />
+      </CTA>
+    </Container>
+  );
 };
 
 export default Login;
