b4b87400b617d3f5fd397a51667eee9be757674d
potchakik <santhonydan7@yahoo.com>
2022-02-19 10:47:52 +0800

    Add Firebase

--- src/components/Detail/style.js
+++ src/components/Detail/style.js
@@ -27,6 +27,7 @@ const ImgTitle = styled.div`
   width: 35vw;
   min-height: 170px;
   min-width: 200px;
+  margin-top: 60px;
 
   img {
     width: 100%;
@@ -95,6 +96,7 @@ const Description = styled.div`
   font-size: 20px;
   margin-top: 16px;
   color: rgb(249, 249, 249);
+  max-width: 760px;
 `;
 
 export {
